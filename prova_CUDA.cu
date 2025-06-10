8eb8e2a7b41ae3c70907fc7941815ddcc5f09cba
Andrea500 <118114963+Andrea500@users.noreply.github.com>
2025-06-10 12:07:12 +0200

    Update prova_CUDA.cu

--- prova_CUDA.cu
+++ prova_CUDA.cu
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
-__global__ void loop(int N)
 
+__global__ void loop(int N)
 {
   int i = threadIdx.x;
-  printf("This is iteration number %d\n"i);
+  printf("This is iteration number %d\n", i);
 }
-
 
 int main()
 {
   int N = 10;
+
   loop<<<1, N>>>(N);
 
   cudaDeviceSynchronize();
